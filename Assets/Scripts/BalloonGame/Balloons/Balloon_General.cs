89741501463dcb475136288906e81245ce228358
dalawrence <dlawrence@unomaha.edu>
2023-10-22 13:20:45 -0500

    REFACTOR: Refactored the Balloon_General code.

--- Assets/Scripts/BalloonGame/Balloons/Balloon_General.cs
+++ Assets/Scripts/BalloonGame/Balloons/Balloon_General.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Balloon_General : MonoBehaviour
 {
     [SerializeField] private float floatStrength;
+    [SerializeField] private int      pointValue;
 
     private void Update()
     {
@@ -18,49 +19,49 @@ public class Balloon_General : MonoBehaviour
     {
         if (other.gameObject.CompareTag("DartPoint"))
         {
-            /* There are more efficient ways of doing this (notice that every time a balloon is 
-               popped we end up calling the destroy method 3 times!), but considering this game
-               is fairly small, I don't think this will be a huge performance hit. We can always 
-               optimize later if needed */
-
-            GameObject audioSource = this.transform.Find("AudioSource").gameObject;
-            /* Decouple the child object from the parent to avoid destroying the parent (along with
-               the child audio object) before the audio is done playing. */
-            audioSource.transform.parent = null;
-            audioSource.GetComponent<AudioSource>().Play();
-            /* Then, make sure to destroy the audio object, but with a delay. */
-            Destroy(audioSource, audioSource.GetComponent<AudioSource>().clip.length);
-
-            /* Same thing as above, but with the particle effects. */
-            GameObject particleEffect = this.transform.Find("ParticleEffects").gameObject;
-            particleEffect.transform.parent = null;
-            particleEffect.GetComponent<ParticleSystem>().Play();
-            Destroy(particleEffect, particleEffect.GetComponent<ParticleSystem>().main.duration); 
-
-            Debug.Log("Balloon hit!");
+            this.PlayEffects();
             BalloonManager.Instance.KillBalloon(gameObject);
-
             DartManager.Instance.DestroyDart(other.gameObject.transform.parent.gameObject);
 
-            PointsManager.addPoints(1);
+            PointsManager.addPoints(this.pointValue);
         }
+    }
+
+    public virtual void PlayEffects()
+    {
+        this.PlaySound();
+        this.PlayParticles();
+    }
+
+    private void PlaySound()
+    {
+        GameObject audioSource = this.transform.Find("AudioSource").gameObject;
+        /* Decouple the child object from the parent to avoid destroying the parent (along with
+           the child audio object) before the audio is done playing. */
+        audioSource.transform.parent = null;
+        audioSource.GetComponent<AudioSource>().Play();
+        /* Then, make sure to destroy the audio object, but with a delay. */
+        Destroy(audioSource, audioSource.GetComponent<AudioSource>().clip.length);
+    }
+
+    private void PlayParticles()
+    {
+        GameObject particleEffect = this.transform.Find("ParticleEffects").gameObject;
+        particleEffect.transform.parent = null;
+        particleEffect.GetComponent<ParticleSystem>().Play();
+        Destroy(particleEffect, particleEffect.GetComponent<ParticleSystem>().main.duration);
     }
 
     /* For testing purposes. Useful for testing on the computer rather than in the headset. */
     public virtual void OnMouseDown()
     {
-        GameObject audioSource = this.transform.Find("AudioSource").gameObject;
-        audioSource.transform.parent = null;
-        audioSource.GetComponent<AudioSource>().Play();
-        Destroy(audioSource, audioSource.GetComponent<AudioSource>().clip.length);
-
-        GameObject particleEffect = this.transform.Find("ParticleEffects").gameObject;
-        particleEffect.transform.parent = null;
-        particleEffect.GetComponent<ParticleSystem>().Play();
-        Destroy(particleEffect, particleEffect.GetComponent<ParticleSystem>().main.duration);
+        Debug.Log("Balloon hit");
         
+        this.PlayEffects();
         BalloonManager.Instance.KillBalloon(gameObject);
-        PointsManager.addPoints(1);
+        Debug.Log(this.pointValue);
+        PointsManager.addPoints(this.pointValue);
+
         Debug.Log("Total points = " + PointsManager.getPoints());
     }
 }
