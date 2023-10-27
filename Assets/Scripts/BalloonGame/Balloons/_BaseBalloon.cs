41bfe617f76260d3e8c95ee922787600fe17f87e
dalawrence <dlawrence@unomaha.edu>
2023-10-27 11:08:59 -0500

    Created a base balloon class that can be extended.

---
+++ Assets/Scripts/BalloonGame/Balloons/_BaseBalloon.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _BaseBalloon : MonoBehaviour
+{
+    [SerializeField] protected float floatStrength;
+    [SerializeField] protected int   pointValue;
+    public GameObject                spawnLocation;
+
+}
