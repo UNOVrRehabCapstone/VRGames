6d8eee29a1c6365f02c5e99675cd04d9877c37ad
dalawrence <dlawrence@unomaha.edu>
2023-12-08 09:58:54 -0600

    Added documentation to the CareerModeLevel class.

--- Assets/Scripts/BalloonGame/CareerModeLevel.cs
+++ Assets/Scripts/BalloonGame/CareerModeLevel.cs
@@ -1,12 +1,22 @@
+/**
+ * The CareerModeLevel class stores the schedule of the spawn of balloons for a level as well as
+ * score for a level.
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CareerModeLevel
 {
-    public int score = 0;
-    public string[] schedule;
+    public int score = 0;      /* The user score of a level. */
+    public string[] schedule;  /* The schedule of balloons.  */
 
+    /**
+     * The CareerModeLevel constructor create a level with the passed in schedule.
+     *
+     * @param schedule The schedule of balloons to spawn.
+     */
     public CareerModeLevel(string[] schedule)
     {
         this.schedule = schedule;
