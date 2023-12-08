a1773973db3a773f4bfc93112ffdcbcc4c3a8ee0
dalawrence <dlawrence@unomaha.edu>
2023-12-08 10:21:42 -0600

    Updated documentation.

--- Assets/Scripts/BalloonGame/Balloons/_BaseBalloon.cs
+++ Assets/Scripts/BalloonGame/Balloons/_BaseBalloon.cs
@@ -1,12 +1,11 @@
-/**
- * The _BaseBalloon is an abstract class that defines the bare minimum for what it means to be 
- * a balloon.
- */
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/**
+ * The _BaseBalloon is an abstract class that defines the bare minimum for what it means to be 
+ * a balloon.
+ */
 public abstract class _BaseBalloon : MonoBehaviour
 {
     [SerializeField] protected float floatStrength;
@@ -14,11 +13,21 @@ public abstract class _BaseBalloon : MonoBehaviour
     [SerializeField] protected int   pointValue;
     protected GameObject             spawnLoc;
 
+    /**
+     * The GetSpawnLoc method returns the spawn location of the balloon.
+     *
+     * @returns A game object which is the spawn location.
+     */
     public GameObject GetSpawnLoc()
     {
         return this.spawnLoc;
     }
 
+    /**
+     * The SetSpawnLoc method sets the spawn location of the balloon.
+     *
+     * @param spawnLoc The spawn location of the balloon.
+     */
     public virtual void SetSpawnLoc(GameObject spawnLoc)
     {
         this.spawnLoc = spawnLoc;
