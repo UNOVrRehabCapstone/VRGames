697d8dcb9ab6914599a588ab9333ef142e1f8094
dalawrence <dlawrence@unomaha.edu>
2023-12-24 17:18:42 -0600

    Renamed BalloonGameManager to GameManager.

---
+++ Assets/Scripts/BalloonGame/Managers/GameManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BalloonsGame 
+{
+	public class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance {get; private set;}
+
+        [SerializeField] protected GameSettingsSO gameSettings;
+
+        private void Awake()
+        {
+            /* Singleton pattern make sure there is only one balloon manager. */
+		    if (Instance != null) {
+			    Debug.LogError("There should only be one balloon manager.");
+                Destroy(this);
+		    } else {
+                Instance = this;
+
+                /* Cast to the appropriate game manager. */
+                if (Instance.gameSettings.gameMode == GameSettingsSO.GameMode.CAREER) {
+                    Instance = (CareerGameManager) Instance;
+                } else if (Instance.gameSettings.gameMode == GameSettingsSO.GameMode.CUSTOM) {
+                    Instance = (CustomGameManager) Instance;
+                } else {
+                    Debug.LogError("Invalid game mode.");
+                }
+            }  
+
+            Debug.Log("Balloon game manager active.");
+        }
+
+        public GameSettingsSO GetGameSettings()
+        {
+            return this.gameSettings;
+        }
+    }
+}
+
