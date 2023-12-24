d377411f97fa0fda55eb955837ed23d62ede2ac3
dalawrence <dlawrence@unomaha.edu>
2023-12-24 17:40:22 -0600

    Created a method handle the event when all lives are lost.

--- Assets/Scripts/BalloonGame/Managers/CustomGameManager.cs
+++ Assets/Scripts/BalloonGame/Managers/CustomGameManager.cs
@@ -1,19 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace BalloonsGame
 {
-	public class CustomGameManager : BalloonGameManager
+	public class CustomGameManager : GameManager
 	{
 		private void Start()
 		{
 			if (this.gameSettings.maxLives < 50) {
-				// Subscribe to watching player lives
+				PlayerManager.Instance.OnAllLivesLost += AllLivesLostHandler;
 			}
 
 			// Subscribe to watching the score
 			//BalloonManager.Instance.StartAutomaticSpawner(3.0f);
+		}
+
+		private void AllLivesLostHandler(object sender, EventArgs e)
+		{
+			this.Restart();
 		}
 	}
 }
