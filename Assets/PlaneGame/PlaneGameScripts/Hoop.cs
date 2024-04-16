54de3eddbeac74bfe70237b7f5532e7626ea1a38
UNOVrRehabCapstone <160982103+UNOVrRehabCapstone@users.noreply.github.com>
2024-04-16 13:45:01 -0500

    Revert "Fixed Plane and Target Destruction and Plane Respawning"

--- Assets/PlaneGame/PlaneGameScripts/Hoop.cs
+++ Assets/PlaneGame/PlaneGameScripts/Hoop.cs
@@ -18,40 +18,14 @@ namespace Classes
             {
                 PointsManager.addPoints( 1 );
                 GetComponentInChildren<ParticleSystem>().Play();
-
-                Debug.Log("Destroying Target");
-                Target.targetsInScene--;
-                gameObject.GetComponent<MeshRenderer>().enabled = false;
-                gameObject.GetComponent<MeshCollider>().enabled = false;
                 foreach (var r in gameObject.GetComponentsInChildren<MeshRenderer>())
                 {
                     r.enabled = false;
                 }
-
-                manager.KillHoop(gameObject);
-                manager.StartSpawningHoops();
-
-
-                Destroy(gameObject, 3.0f);
+                //manager.KillHoop(gameObject);
+                //manager.StartSpawningHoops();
             }
         }
-        */
-        public void HitTarget()
-        {
-            GetComponentInChildren<ParticleSystem>().Play();
-
-            Debug.Log("DestroyingTarget");
-            Target.targetsInScene--;
-            gameObject.GetComponent<MeshRenderer>().enabled = false;
-            gameObject.GetComponent<MeshCollider>().enabled = false;
-
-            foreach (var r in gameObject.GetComponentsInChildren<MeshRenderer>())
-            {
-                r.enabled = false;
-            }
-
-            Destroy(gameObject, 3.0f);
-            PointsManager.addPoints(1);
     }
 }
 
